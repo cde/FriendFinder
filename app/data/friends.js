1e336befe95379c636b69891ab92cd2988be07c4
cde <cdechauri@gmail.com>
2018-08-18 18:01:20 -0700

    Add best match logic

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friend list.
+// Initially we just set it equal to a "dummy" friend.
+// ==
+
+var friendsList = [
+    {
+        "name":"Linda",
+        "photo":"https://cdn.stocksnap.io/img-thumbs/960w/FIGLTLWW8E.jpg",
+        "scores":[ 5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name":"Terrence",
+        "photo":"https://cdn.stocksnap.io/img-thumbs/960w/DVXSK4K9ZV.jpg",
+        "scores":[ 5,5,5,5,5,5,5,5,5,5]
+    },
+
+    {
+        "name":"Anna",
+        "photo":"https://cdn.stocksnap.io/img-thumbs/960w/MJ4OQG4PKI.jpg",
+        "scores":[ 5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name":"George",
+        "photo":"https://cdn.stocksnap.io/img-thumbs/960w/SW0YN0Z5T0.jpg",
+        "scores":[ 5,3,4,2,5,1,3,5,4,3]
+    },
+
+    ]
+
+module.exports = friendsList;
+
+
+// {
+//     "name":"Jeremy",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/5RMWOECEX3.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
+// {
+//     "name":"Jeremy",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/5RMWOECEX3.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
+// {
+//     "name":"Terrence",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/DVXSK4K9ZV.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
+//
+// {
+//     "name":"Anna",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/MJ4OQG4PKI.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
+//
+// {
+//     "name":"George",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/SW0YN0Z5T0.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
+//
+// {
+//     "name":"George",
+//     "photo":"https://cdn.stocksnap.io/img-thumbs/960w/SW0YN0Z5T0.jpg",
+//     "scores":[ 5,1,4,4,5,1,2,5,4,1]
+// },
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,62 @@
+// Helper methods
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const getRandomIndex = (max) =>{
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 
-module.exports = function(app){
+let friendsList = require("../data/friends");
+
+function apiRoutes(app){
     app.get("/api/friends", function(req,res){
-
+        res.json(friendsList);
     })
 
-// Create New Characters - takes in JSON input
+    // Calculate the best match
     app.post("/api/friends", function(req, res) {
+        let userData = req.body;
+        let userDataScores = userData.scores;
 
+        let friendsDiff = friendsList.map(function(friend){
+            // Compare the difference between current user's scores against those from other users, question by question
+            let diffScores = []
+            for(let i=0; i < friend["scores"].length; i++){
+                diffScores.push(Math.abs(friend["scores"][i] - userDataScores[i]));
+            }
+            // console.log(`${friend["name"]} diff scores`, diffScores)
+
+            // Get the total diff & store it with user's data
+            let totalDiff = diffScores.reduce(reducer);
+            return { name: friend["name"], photo: friend["photo"], diff: totalDiff }
+
+        })
+
+        // Find the closest match (Min diff) by:
+        // 1) Sorting
+        // friendsDiff.sort(function (a, b) {
+        //     return a.diff - b.diff;
+        // })
+        // let diff = friendsDiff[0].diff
+
+        // 2) or Find Min()
+        let diff = Math.min(...friendsDiff.map(friend => { return friend["diff"] }));
+
+        const bestMatches = friendsDiff.filter(friend => {
+            return friend["diff"] === diff
+        });
+        // If we get more than one matches, return one randomly
+        let bestMatch = {}
+        if(bestMatches.length > 1) {
+            let index = getRandomIndex(bestMatches.length)
+            bestMatch = bestMatches[index]
+            console.log(`${bestMatches[index]} diff scores`, index)
+        }
+
+        if(bestMatches.length == 1){
+            bestMatch = bestMatches[0]
+        }
+        res.json(bestMatch);
     });
 }
+
+module.exports =  apiRoutes;
